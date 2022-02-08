1b79fbb051a9d11a0e8a7c223c3ed9f27f381637
Wacil23 <wacilzek00@gmail.com>
2022-02-08 15:47:19 +0100

    Finish

--- variable3.php
+++ variable3.php
@@ -1,2 +1,11 @@
 <?php
+    $booksSteven["L'ile au trésor"] = '1883';
+    $booksSteven["Catriona"] = '1893';
+    $booksSteven["Enlevé"] = '1886'; 
+   
     
+    asort($booksSteven);
+
+    foreach ($booksSteven as $key => $value){
+        echo ($key. ' '. $value . '/n');
+    }
